5061e387bf34aa0aa053112f8537bf23b953ff7e
Chris MacNaughton <chmacnaughton@gmail.com>
2015-10-22 16:44:07 -0400

    log to carbong

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate yaml_rust;
 #[macro_use] extern crate log;
 extern crate simple_logger;
 extern crate influent;
-
+use std::net::TcpStream;
 use rustc_serialize::json::Json;
 use yaml_rust::YamlLoader;
 use std::fs::File;
@@ -29,7 +29,7 @@ macro_rules! parse_opt (
 
 #[derive(Clone,Debug)]
 struct Args {
-    carbon: Option<String>,
+    carbon: Option<Carbon>,
     elasticsearch: Option<String>,
     stdout: Option<String>,
     influx: Option<Influx>,
@@ -41,6 +41,12 @@ struct Influx {
     password: String,
     host: String,
     port: String
+}
+#[derive(Clone,Debug)]
+struct Carbon {
+    host: String,
+    port: String,
+    root_key: String,
 }
 
 impl Args {
@@ -71,8 +77,7 @@ fn get_config() -> Result<Args, String>{
     };
 
     let doc = &docs[0];
-    parse_opt!(carbon, doc["carbon"]);
-    // parse_opt!(elasticsearch, doc["elasticsearch"]);
+
     let elasticsearch = match doc["elasticsearch"].as_str() {
         Some(o) => Some(format!("http://{}/ceph/operations", o)),
         None => None
@@ -88,6 +93,25 @@ fn get_config() -> Result<Args, String>{
         port: influx_port.to_string(),
         password: influx_password.to_string(),
         user: influx_user.to_string(),
+    };
+
+    let carbon_doc = doc["carbon"].clone();
+
+    let carbon_host = match carbon_doc["host"].as_str(){
+        Some(h) => Some(h),
+        None => None,
+    };
+
+    let carbon_port = carbon_doc["port"].as_str().unwrap_or("2003");
+    let root_key = carbon_doc["root_key"].as_str().unwrap_or("ceph");
+
+    let carbon = match carbon_host {
+        Some(h) => Some(Carbon {
+                host: h.to_string(),
+                port: carbon_port.to_string(),
+                root_key: root_key.to_string(),
+            }),
+        None => None
     };
 
     let outputs: Vec<String> = match doc["outputs"].as_vec() {
@@ -131,8 +155,9 @@ fn get_time()->f64{
 impl CephHealth{
     fn to_json(&self)->String{
 
-        format!("{{\"ops_per_sec\": \"{}\",\"write_bytes_sec\": \"{}\", \"read_bytes_sec\": \"{}\", \"data\":\"{}\", \
+        format!("{{\"osds\": \"{}\", \"ops_per_sec\": \"{}\",\"write_bytes_sec\": \"{}\", \"read_bytes_sec\": \"{}\", \"data\":\"{}\", \
             \"bytes_used\":{}, \"bytes_avail\":{}, \"bytes_total\":\"{}\", \"postDate\": {}}}",
+            self.num_osds,
             self.ops,
             self.write_bytes_sec,
             self.read_bytes_sec,
@@ -141,6 +166,20 @@ impl CephHealth{
             self.bytes_avail,
             self.bytes_total,
             get_time())
+    }
+
+    fn to_carbon_string(&self, root_key: &String) -> String {
+        format!( r#"{root_key}.{} {} {timestamp}
+{root_key}.{} {} {timestamp}
+{root_key}.{} {} {timestamp}
+{root_key}.{} {} {timestamp}
+{root_key}.{} {} {timestamp}
+{root_key}.{} {} {timestamp}
+{root_key}.{} {} {timestamp}
+{root_key}.{} {} {timestamp}
+"#, "osds", self.num_osds, "ops", self.ops, "write_bytes", self.write_bytes_sec,
+"read_bytes", self.read_bytes_sec, "data", self.data, "used", self.bytes_used,
+"avail", self.bytes_avail, "total", self.bytes_total, root_key = root_key.clone(), timestamp = get_time())
     }
 }
 
@@ -182,7 +221,7 @@ fn parse_i64(num: Result<rustc_serialize::json::Json, f64>) -> i64 {
 
 fn get_ceph_stats() -> Result<String, String> {
     // return Ok("{\"health\":{\"health\":{\"health_services\":[{\"mons\":[{\"name\":\"chris-local-machine-1\",\"kb_total\":232205304,\"kb_used\":81823684,\"kb_avail\":138563228,\"avail_percent\":59,\"last_updated\":\"2015-10-07 12:19:51.281273\",\"store_stats\":{\"bytes_total\":5408347,\"bytes_sst\":0,\"bytes_log\":4166001,\"bytes_misc\":1242346,\"last_updated\":\"0.000000\"},\"health\":\"HEALTH_OK\"},{\"name\":\"chris-local-machine-2\",\"kb_total\":232205304,\"kb_used\":79803236,\"kb_avail\":140583676,\"avail_percent\":60,\"last_updated\":\"2015-10-07 12:19:23.247120\",\"store_stats\":{\"bytes_total\":6844874,\"bytes_sst\":0,\"bytes_log\":5602535,\"bytes_misc\":1242339,\"last_updated\":\"0.000000\"},\"health\":\"HEALTH_OK\"},{\"name\":\"chris-local-machine-3\",\"kb_total\":232205304,\"kb_used\":78650196,\"kb_avail\":141736716,\"avail_percent\":61,\"last_updated\":\"2015-10-07 12:19:07.182466\",\"store_stats\":{\"bytes_total\":6531182,\"bytes_sst\":0,\"bytes_log\":5288894,\"bytes_misc\":1242288,\"last_updated\":\"0.000000\"},\"health\":\"HEALTH_OK\"}]}]},\"summary\":[],\"timechecks\":{\"epoch\":6,\"round\":38,\"round_status\":\"finished\",\"mons\":[{\"name\":\"chris-local-machine-1\",\"skew\":\"0.000000\",\"latency\":\"0.000000\",\"health\":\"HEALTH_OK\"},{\"name\":\"chris-local-machine-2\",\"skew\":\"0.000000\",\"latency\":\"0.000977\",\"health\":\"HEALTH_OK\"},{\"name\":\"chris-local-machine-3\",\"skew\":\"0.000000\",\"latency\":\"0.000818\",\"health\":\"HEALTH_OK\"}]},\"overall_status\":\"HEALTH_OK\",\"detail\":[]},\"fsid\":\"1bb15abc-4158-11e5-b499-00151737cf98\",\"election_epoch\":6,\"quorum\":[0,1,2],\"quorum_names\":[\"chris-local-machine-1\",\"chris-local-machine-2\",\"chris-local-machine-3\"],\"monmap\":{\"epoch\":2,\"fsid\":\"1bb15abc-4158-11e5-b499-00151737cf98\",\"modified\":\"2015-10-07 10:45:23.255204\",\"created\":\"0.000000\",\"mons\":[{\"rank\":0,\"name\":\"chris-local-machine-1\",\"addr\":\"10.0.2.22:6789/0\"},{\"rank\":1,\"name\":\"chris-local-machine-2\",\"addr\":\"10.0.2.78:6789/0\"},{\"rank\":2,\"name\":\"chris-local-machine-3\",\"addr\":\"10.0.2.141:6789/0\"}]},\"osdmap\":{\"osdmap\":{\"epoch\":9,\"num_osds\":3,\"num_up_osds\":3,\"num_in_osds\":3,\"full\":false,\"nearfull\":false}},\"pgmap\":{\"pgs_by_state\":[{\"state_name\":\"active+clean\",\"count\":192}],\"version\":487,\"num_pgs\":192,\"data_bytes\":4970896648,\"bytes_used\":252251439104,\"bytes_avail\":424777154560,\"bytes_total\":713334693888,\"write_bytes_sec\":26793300,\"op_per_sec\":8},\"mdsmap\":{\"epoch\":1,\"up\":0,\"in\":0,\"max\":1,\"by_rank\":[]}}".to_string());
-    // return Ok("{\"health\":{\"health\":{\"health_services\":[{\"mons\":[{\"name\":\"chris-local-machine-1\",\"kb_total\":232205304,\"kb_used\":81823684,\"kb_avail\":138563228,\"avail_percent\":59,\"last_updated\":\"2015-10-07 12:19:51.281273\",\"store_stats\":{\"bytes_total\":5408347,\"bytes_sst\":0,\"bytes_log\":4166001,\"bytes_misc\":1242346,\"last_updated\":\"0.000000\"},\"health\":\"HEALTH_OK\"},{\"name\":\"chris-local-machine-2\",\"kb_total\":232205304,\"kb_used\":79803236,\"kb_avail\":140583676,\"avail_percent\":60,\"last_updated\":\"2015-10-07 12:19:23.247120\",\"store_stats\":{\"bytes_total\":6844874,\"bytes_sst\":0,\"bytes_log\":5602535,\"bytes_misc\":1242339,\"last_updated\":\"0.000000\"},\"health\":\"HEALTH_OK\"},{\"name\":\"chris-local-machine-3\",\"kb_total\":232205304,\"kb_used\":78650196,\"kb_avail\":141736716,\"avail_percent\":61,\"last_updated\":\"2015-10-07 12:19:07.182466\",\"store_stats\":{\"bytes_total\":6531182,\"bytes_sst\":0,\"bytes_log\":5288894,\"bytes_misc\":1242288,\"last_updated\":\"0.000000\"},\"health\":\"HEALTH_OK\"}]}]},\"summary\":[],\"timechecks\":{\"epoch\":6,\"round\":38,\"round_status\":\"finished\",\"mons\":[{\"name\":\"chris-local-machine-1\",\"skew\":\"0.000000\",\"latency\":\"0.000000\",\"health\":\"HEALTH_OK\"},{\"name\":\"chris-local-machine-2\",\"skew\":\"0.000000\",\"latency\":\"0.000977\",\"health\":\"HEALTH_OK\"},{\"name\":\"chris-local-machine-3\",\"skew\":\"0.000000\",\"latency\":\"0.000818\",\"health\":\"HEALTH_OK\"}]},\"overall_status\":\"HEALTH_OK\",\"detail\":[]},\"fsid\":\"1bb15abc-4158-11e5-b499-00151737cf98\",\"election_epoch\":6,\"quorum\":[0,1,2],\"quorum_names\":[\"chris-local-machine-1\",\"chris-local-machine-2\",\"chris-local-machine-3\"],\"monmap\":{\"epoch\":2,\"fsid\":\"1bb15abc-4158-11e5-b499-00151737cf98\",\"modified\":\"2015-10-07 10:45:23.255204\",\"created\":\"0.000000\",\"mons\":[{\"rank\":0,\"name\":\"chris-local-machine-1\",\"addr\":\"10.0.2.22:6789/0\"},{\"rank\":1,\"name\":\"chris-local-machine-2\",\"addr\":\"10.0.2.78:6789/0\"},{\"rank\":2,\"name\":\"chris-local-machine-3\",\"addr\":\"10.0.2.141:6789/0\"}]},\"osdmap\":{\"osdmap\":{\"epoch\":9,\"num_osds\":3,\"num_up_osds\":3,\"num_in_osds\":3,\"full\":false,\"nearfull\":false}},\"pgmap\":{\"pgs_by_state\":[{\"state_name\":\"active+clean\",\"count\":192}],\"version\":487,\"num_pgs\":192,\"data_bytes\":4970896648,\"bytes_used\":252251439104,\"bytes_avail\":424777154560,\"bytes_total\":713334693888,\"write_bytes_sec\":26793300},\"mdsmap\":{\"epoch\":1,\"up\":0,\"in\":0,\"max\":1,\"by_rank\":[]}}".to_string());
+    // return Ok("{\"health\":{\"health\":{\"health_services\":[{\"mons\":[{\"name\":\"ip-172-31-3-4\",\"kb_total\":257899908,\"kb_used\":2646276,\"kb_avail\":244667856,\"avail_percent\":94,\"last_updated\":\"2015-10-21 17:29:49.157456\",\"store_stats\":{\"bytes_total\":4211748,\"bytes_sst\":0,\"bytes_log\":2328812,\"bytes_misc\":1882936,\"last_updated\":\"0.000000\"},\"health\":\"HEALTH_OK\"},{\"name\":\"ip-172-31-18-59\",\"kb_total\":257899908,\"kb_used\":2626376,\"kb_avail\":244687756,\"avail_percent\":94,\"last_updated\":\"2015-10-21 17:29:55.825254\",\"store_stats\":{\"bytes_total\":5364733,\"bytes_sst\":0,\"bytes_log\":3481648,\"bytes_misc\":1883085,\"last_updated\":\"0.000000\"},\"health\":\"HEALTH_OK\"},{\"name\":\"ip-172-31-55-150\",\"kb_total\":257899908,\"kb_used\":2732484,\"kb_avail\":244581648,\"avail_percent\":94,\"last_updated\":\"2015-10-21 17:30:22.606563\",\"store_stats\":{\"bytes_total\":5470059,\"bytes_sst\":0,\"bytes_log\":3586875,\"bytes_misc\":1883184,\"last_updated\":\"0.000000\"},\"health\":\"HEALTH_OK\"}]}]},\"summary\":[],\"timechecks\":{\"epoch\":6,\"round\":64,\"round_status\":\"finished\",\"mons\":[{\"name\":\"ip-172-31-3-4\",\"skew\":\"0.000000\",\"latency\":\"0.000000\",\"health\":\"HEALTH_OK\"},{\"name\":\"ip-172-31-18-59\",\"skew\":\"-0.001446\",\"latency\":\"0.119155\",\"health\":\"HEALTH_OK\"},{\"name\":\"ip-172-31-55-150\",\"skew\":\"-0.005493\",\"latency\":\"0.003979\",\"health\":\"HEALTH_OK\"}]},\"overall_status\":\"HEALTH_OK\",\"detail\":[]},\"fsid\":\"1bb15abc-4158-11e5-b499-00151737cf98\",\"election_epoch\":6,\"quorum\":[0,1,2],\"quorum_names\":[\"ip-172-31-3-4\",\"ip-172-31-18-59\",\"ip-172-31-55-150\"],\"monmap\":{\"epoch\":2,\"fsid\":\"1bb15abc-4158-11e5-b499-00151737cf98\",\"modified\":\"2015-10-21 14:51:21.352722\",\"created\":\"0.000000\",\"mons\":[{\"rank\":0,\"name\":\"ip-172-31-3-4\",\"addr\":\"172.31.3.4:6789/0\"},{\"rank\":1,\"name\":\"ip-172-31-18-59\",\"addr\":\"172.31.18.59:6789/0\"},{\"rank\":2,\"name\":\"ip-172-31-55-150\",\"addr\":\"172.31.55.150:6789/0\"}]},\"osdmap\":{\"osdmap\":{\"epoch\":22,\"num_osds\":8,\"num_up_osds\":8,\"num_in_osds\":8,\"full\":false,\"nearfull\":false}},\"pgmap\":{\"pgs_by_state\":[{\"state_name\":\"active+clean\",\"count\":192}],\"version\":5467,\"num_pgs\":192,\"data_bytes\":1072168960,\"bytes_used\":22150647808,\"bytes_avail\":2003846721536,\"bytes_total\":2112716046336},\"mdsmap\":{\"epoch\":1,\"up\":0,\"in\":0,\"max\":1,\"by_rank\":[]}}".to_string());
     let output = Command::new("/usr/bin/ceph")
                          .arg("-s")
                          .arg("-f")
@@ -278,9 +317,28 @@ fn log_to_influx(args: &Args, ceph_event: &CephHealth) {
     }
 }
 
+fn log_packet_to_carbon(carbon_url: &str, carbon_data: String) -> Result<(), String> {
+    let mut stream = try!(TcpStream::connect(carbon_url).map_err(|e| e.to_string()));
+    let bytes_written = try!(stream.write(&carbon_data.into_bytes()[..]).map_err(|e| e.to_string()));
+    info!("Wrote: {} bytes to graphite", &bytes_written);
+    Ok(())
+}
+
+fn log_to_carbon(args: &Args, ceph_event: &CephHealth){
+    if args.outputs.contains(&"carbon".to_string()) && args.carbon.is_some() {
+        let carbon = &args.carbon.clone().unwrap();
+        let carbon_data = ceph_event.to_carbon_string(&carbon.root_key);
+
+        let carbon_host = &carbon.host;
+        let carbon_port = &carbon.port;
+        let carbon_url = format!("{}:{}", carbon_host, carbon_port);
+        let _ = log_packet_to_carbon(carbon_url.as_ref(), carbon_data);
+    }
+}
+
 fn main() {
     //TODO make configurable via cli or config arg
-    simple_logger::init_with_level(LogLevel::Info).unwrap();
+    simple_logger::init_with_level(LogLevel::Debug).unwrap();
 
     let periodic = timer_periodic(1000);
 
@@ -296,7 +354,13 @@ fn main() {
             Err(_) => "{}".to_string(),
         };
 
-        let obj = Json::from_str(json.as_ref()).unwrap();
+        let obj = match Json::from_str(json.as_ref()) {
+            Ok(obj) => obj,
+            Err(e) => {
+                warn!("There was a problem: {:?}", e);
+                Json::from_str("{}").unwrap()
+            }
+        };
         // println!("{:?}", obj);
 
         let ceph_event = CephHealth {
@@ -313,6 +377,7 @@ fn main() {
         log_to_es(&args, &ceph_event);
         log_to_stdout(&args, &ceph_event);
         log_to_influx(&args, &ceph_event);
+        log_to_carbon(&args, &ceph_event);
     }
 
 }
